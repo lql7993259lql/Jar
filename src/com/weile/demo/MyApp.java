76b6af0207725ffc54b8fd17a1f304ecb35a34da
刘清林 <liuqinglin@anne.com.cn>
2018-04-08 00:04:02 +0800

    Initial commit

---
+++ src/com/weile/demo/MyApp.java
@@ -0,0 +1,18 @@
+package com.weile.demo;
+
+public class MyApp {
+	public static String Add(){
+		return "Addqweqwe";
+	}
+	
+	
+	public static String Plus1(){
+		return "plus1";
+	}
+	public static String Plus2(){
+		return "plus2";
+	}
+	public static String Plus3(){
+		return "plus3";
+	}
+}
---
+++ src/com/weile/demo/Test.java
@@ -0,0 +1,13 @@
+package com.weile.demo;
+
+public class Test {
+	public static String Plus1(){
+		return "plus1";
+	}
+	public static String Plus2(){
+		return "plus2";
+	}
+	public static String Plus3(){
+		return "plus3";
+	}
+}
